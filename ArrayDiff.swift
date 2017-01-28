8b8df5c116bb2894b0066d0e31c3ef4d672928dc
Eduardo Arenas <eduardo.arenas@gamechanger.io>
2017-01-28 10:47:25 -0500

    Initial commit

---
+++ ArrayDiff.swift
@@ -0,0 +1,103 @@
+//
+//  CollectionDiff.swift
+//  TennisScoring
+//
+//  Created by Eduardo Arenas on 1/1/17.
+//  Copyright © 2017 eapdev. All rights reserved.
+//
+
+import Foundation
+
+struct DiffItem<T> {
+  let index: Int
+  let element: T
+}
+
+struct Diff<T> {
+  let additions: [DiffItem<T>]
+  let deletions: [DiffItem<T>]
+}
+
+extension Array where Element: Equatable {
+
+  func diff(other: Array<Element>) -> Diff<Element> {
+    let reducedArrays = nonMatchingSubArray(old: self, new: other)
+    let lcsTable = lcsLengthTable(old: reducedArrays.old, new: reducedArrays.new)
+    return diffFromLCSTable(table: lcsTable, old: reducedArrays.old, new: reducedArrays.new, indexOffset: reducedArrays.startIndex)
+  }
+}
+
+private func nonMatchingSubArray<T: Equatable>(old: [T], new: [T]) -> (old: [T], new: [T], startIndex: Int) {
+  guard old.count > 0 && new.count > 0 else {
+    return (old, new, 0)
+  }
+  var startIndex = 0
+  var oldEndIndex = old.count - 1
+  var newEndIndex = new.count - 1
+
+  while startIndex < oldEndIndex && startIndex < newEndIndex && old[startIndex] == new[startIndex] {
+    startIndex += 1
+  }
+
+  while oldEndIndex > startIndex && newEndIndex > startIndex && old[oldEndIndex] == new[newEndIndex] {
+    oldEndIndex -= 1
+    newEndIndex -= 1
+  }
+
+  return (Array(old[startIndex...oldEndIndex]), Array(new[startIndex...newEndIndex]), startIndex)
+}
+
+private struct TableItem {
+  let length: Int
+  let previousIndex: (x: Int, y: Int)?
+}
+
+private func lcsLengthTable<T: Equatable>(old: [T], new: [T]) -> [[TableItem]] {
+  let n = old.count
+  let m = new.count
+  var table = Array<[TableItem]>(repeating: Array(repeating: TableItem(length: 0, previousIndex: nil), count: m + 1), count: n + 1)
+
+  for i in 0...n {
+    for j in 0...m {
+      if i == 0 && j == 0 {
+        table[i][j] = TableItem(length: 0, previousIndex: nil)
+      } else if i == 0 {
+        table[i][j] = TableItem(length: 0, previousIndex: (i, j - 1))
+      } else if j == 0 {
+        table[i][j] = TableItem(length: 0, previousIndex: (i - 1, j))
+      } else if old[i - 1] == new[j - 1] {
+        table[i][j] = TableItem(length: table[i - 1][j - 1].length + 1, previousIndex: (i - 1, j - 1))
+      } else {
+        if table[i][j - 1].length >= table[i - 1][j].length {
+          table[i][j] = TableItem(length: table[i][j - 1].length, previousIndex: (i, j - 1))
+        } else {
+          table[i][j] = TableItem(length: table[i - 1][j].length, previousIndex: (i - 1, j))
+        }
+      }
+    }
+  }
+
+  return table
+}
+
+private func diffFromLCSTable<T: Equatable>(table: [[TableItem]], old: [T], new: [T], indexOffset: Int) -> Diff<T> {
+  var currentIndex = (x: table.count - 1, y: table[0].count - 1)
+  var currentItem = table[currentIndex.x][currentIndex.y]
+
+  var additions = [DiffItem<T>]()
+  var deletions = [DiffItem<T>]()
+
+  while let previousIndex = currentItem.previousIndex {
+
+    if previousIndex.x == currentIndex.x && previousIndex.y < currentIndex.y {
+      additions.append(DiffItem(index: currentIndex.y - 1 + indexOffset, element: new[currentIndex.y - 1]))
+    } else if previousIndex.x < currentIndex.x && previousIndex.y == currentIndex.y {
+      deletions.append(DiffItem(index: currentIndex.x - 1 + indexOffset, element: old[currentIndex.x - 1]))
+    }
+
+    currentIndex = previousIndex
+    currentItem = table[currentIndex.x][currentIndex.y]
+  }
+
+  return Diff(additions: additions, deletions: deletions)
+}
---
+++ ArrayDiffTests.swift
@@ -0,0 +1,357 @@
+//
+//  ArrayDiffTests.swift
+//  TennisScoring
+//
+//  Created by Eduardo Arenas on 1/1/17.
+//  Copyright © 2017 eapdev. All rights reserved.
+//
+
+import XCTest
+@testable import TennisScoring
+
+class ArrayDiffTests: XCTestCase {
+
+  func testCompleteInsertion() {
+    let diff = [Int]().diff(other: [1, 2, 3])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 3}))
+  }
+
+  func testCompleteDeletion() {
+    let diff = [1, 2, 3].diff(other: [])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+  }
+
+  func testSingleMiddleInsertion() {
+    let diff = [1, 3, 4].diff(other: [1, 2, 3, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertEqual(diff.additions[0].index, 1)
+    XCTAssertEqual(diff.additions[0].element, 2)
+  }
+
+  func testSingleMiddleDeletion() {
+    let diff = [1, 2, 3, 4].diff(other: [1, 2, 4])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.deletions[0].index, 2)
+    XCTAssertEqual(diff.deletions[0].element, 3)
+  }
+
+  func testSingleMiddleReplacement() {
+    let diff = [1, 3, 4].diff(other: [1, 2, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.additions[0].index, 1)
+    XCTAssertEqual(diff.additions[0].element, 2)
+    XCTAssertEqual(diff.deletions[0].index, 1)
+    XCTAssertEqual(diff.deletions[0].element, 3)
+  }
+
+  func testSingleStartInsertion() {
+    let diff = [2, 3, 4].diff(other: [1, 2, 3, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertEqual(diff.additions[0].index, 0)
+    XCTAssertEqual(diff.additions[0].element, 1)
+  }
+
+  func testSingleStartDeletion() {
+    let diff = [1, 2, 3, 4].diff(other: [2, 3, 4])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.deletions[0].index, 0)
+    XCTAssertEqual(diff.deletions[0].element, 1)
+  }
+
+  func testSingleStartReplacement() {
+    let diff = [1, 3, 4].diff(other: [2, 3, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.additions[0].index, 0)
+    XCTAssertEqual(diff.additions[0].element, 2)
+    XCTAssertEqual(diff.deletions[0].index, 0)
+    XCTAssertEqual(diff.deletions[0].element, 1)
+  }
+
+  func testSingleEndInsertion() {
+    let diff = [1, 2, 3].diff(other: [1, 2, 3, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertEqual(diff.additions[0].index, 3)
+    XCTAssertEqual(diff.additions[0].element, 4)
+  }
+
+  func testSingleEndDeletion() {
+    let diff = [1, 2, 3, 4].diff(other: [1, 2, 3])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.deletions[0].index, 3)
+    XCTAssertEqual(diff.deletions[0].element, 4)
+  }
+
+  func testSingleEndReplacement() {
+    let diff = [1, 2, 3].diff(other: [1, 2, 4])
+    XCTAssertEqual(diff.additions.count, 1)
+    XCTAssertEqual(diff.deletions.count, 1)
+    XCTAssertEqual(diff.additions[0].index, 2)
+    XCTAssertEqual(diff.additions[0].element, 4)
+    XCTAssertEqual(diff.deletions[0].index, 2)
+    XCTAssertEqual(diff.deletions[0].element, 3)
+  }
+
+  func testMultipleConsecutiveMiddleInsertion() {
+    let diff = [1, 2, 3].diff(other: [1, 2, 4, 5, 6, 3])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 4}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 5}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 6}))
+  }
+
+  func testMultipleConsecutiveMiddleDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 5, 6])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+  }
+
+  func testMultipleConsecutiveMiddleReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 2, 7, 8, 9, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 4 && $0.element == 5}))
+  }
+
+  func testMultipleNonConsecutiveMiddleInsertion() {
+    let diff = [1, 2, 3].diff(other: [1, 4, 5, 2, 6, 3])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 4}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 5}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 6}))
+  }
+
+  func testMultipleNonConsecutiveMiddleDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 4, 6])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 4 && $0.element == 5}))
+  }
+
+  func testMultipleNonConsecutiveMiddleReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 7, 8, 4, 9, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 4 && $0.element == 5}))
+  }
+
+  func testMultiplConsecutiveStartInsertion() {
+    let diff = [4, 5, 6].diff(other: [1, 2, 3, 4, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 3}))
+  }
+
+  func testMultipleConsecutiveStartDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [4, 5, 6])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+  }
+
+  func testMultipleConsecutiveStartReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [7, 8, 9, 4, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 0 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+  }
+
+  func testMultipleNonConsecutiveStartInsertion() {
+    let diff = [4, 5, 6].diff(other: [1, 2, 4, 3, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 3}))
+  }
+
+  func testMultipleNonConsecutiveStartDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [3, 5, 6])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+  }
+
+  func testMultipleNonConsecutiveStartReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [7, 8, 3, 9, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 0 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 1 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 1 && $0.element == 2}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+  }
+
+  func testMultiplConsecutiveEndInsertion() {
+    let diff = [1, 2, 3].diff(other: [1, 2, 3, 4, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 5}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testMultipleConsecutiveEndDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 2, 3])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 4 && $0.element == 5}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testMultipleConsecutiveEndReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 2, 3, 7, 8, 9])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 5 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 4 && $0.element == 5}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testMultipleNonConsecutiveEndInsertion() {
+    let diff = [1, 2, 4].diff(other: [1, 2, 3, 4, 5, 6])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 0)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 4 && $0.element == 5}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testMultipleNonConsecutiveEndDeletion() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 2, 5])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testMultipleNonConsecutiveEndReplacement() {
+    let diff = [1, 2, 3, 4, 5, 6].diff(other: [1, 2, 7, 8, 5, 9])
+    XCTAssertEqual(diff.additions.count, 3)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 2 && $0.element == 7}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 3 && $0.element == 8}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 5 && $0.element == 9}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 2 && $0.element == 3}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 3 && $0.element == 4}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 5 && $0.element == 6}))
+  }
+
+  func testDuplicatedMidSectionDeletion() {
+    let diff = [1, 2, 3, 3, 3, 5, 6].diff(other: [1, 2, 3, 5, 6])
+    XCTAssertEqual(diff.additions.count, 0)
+    XCTAssertEqual(diff.deletions.count, 2)
+    XCTAssertEqual(diff.deletions.filter({ $0.index >= 2 && $0.index <= 4 }).count, 2)
+    XCTAssertEqual(diff.deletions.filter({ $0.element == 3 }).count, 2)
+  }
+
+  func testLargeSequenceDiff() {
+    let old = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+
+    let new = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 55, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101]
+
+    let diff = old.diff(other: new)
+
+    XCTAssertEqual(diff.additions.count, 2)
+    XCTAssertEqual(diff.deletions.count, 3)
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 0 && $0.element == 1}))
+    XCTAssertTrue(diff.deletions.contains(where: { $0.index == 44 && $0.element == 45}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 53 && $0.element == 55}))
+    XCTAssertTrue(diff.additions.contains(where: { $0.index == 98 && $0.element == 101}))
+  }
+
+//  func testLargeSequenceDiffPerformance() {
+//    let old = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+//
+//    let new = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 55, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101]
+//
+//    measure() {
+//      let _ = old.diff(other: new)
+//    }
+//  }
+//
+//  func testHugeSequenceDiffPerformanceWhenEqual() {
+//    let old = Array(1...5000)
+//    let new = Array(1...5000)
+//
+//    measure() {
+//      let _ = old.diff(other: new)
+//    }
+//  }
+//
+//
+//  func testHugeSequenceDiffPerformanceWithProximateChanges() {
+//    let old = Array(1...5000)
+//    var new = Array(1...5000)
+//
+//    new.removeSubrange(3000...3003)
+//    new.insert(contentsOf: [5000, 50001], at: 3005)
+//
+//    measure() {
+//      let _ = old.diff(other: new)
+//    }
+//  }
+//
+//  func testHugeSequenceDiffPerformanceWithASparseChanges() {
+//    let old = Array(1...5000)
+//    var new = Array(1...5000)
+//
+//    new.remove(at: 2)
+//    new.insert(contentsOf: [5000, 50001], at: 4985)
+//    new.replaceSubrange(2500...2503, with: [6000, 60001, 60002])
+//
+//    measure() {
+//      let _ = old.diff(other: new)
+//    }
+//  }
+}
